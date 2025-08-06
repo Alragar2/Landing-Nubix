f18fef198be12909cb6039b40923fb86bd7abba3
alvaro <alragar2@alumni.uv.es>
2025-08-06 19:11:48 +0200

    Prueba

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -60,7 +60,7 @@ const About = () => {
                   Nuestra Historia
                 </h3>
                 <p className="text-gray-600 mb-4 leading-relaxed">
-                  Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus 
+                  Jorge ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus 
                   ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis 
                   convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla 
                   lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.
